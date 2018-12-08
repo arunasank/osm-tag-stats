563cae6f52dcbaba58020daa834ead947338de9b
Aruna Sankaranarayanan <aruna.evam@gmail.com>
2018-12-08 21:59:19 +0530

    latest ucb

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 'use strict';
+process.env.year = 2009;
+process.env.q = 2;
+process.env.tileset = '36087';
+const help = require('./util/help.js');
+const tileReduce = require('@mapbox/tile-reduce');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+const turf = require('turf');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+const tmpDir = 'tmp-osm-tag-stats/';
+const cleanArguments = require('./util/cleanArguments')(argv, tmpDir);
 
-var help = require('./util/help.js');
-var tileReduce = require('tile-reduce');
-var path = require('path');
-var argv = require('minimist')(process.argv.slice(2));
-var gsm = require('geojson-stream-merge');
-var fs = require('fs');
-
-var tmpDir = 'tmp-osm-tag-stats/';
-var cleanArguments = require('./util/cleanArguments')(argv, tmpDir);
-
-var count = cleanArguments.argv.count,
-    geojson = cleanArguments.argv.geojson,
+const count = cleanArguments.argv.count,
     users = cleanArguments.argv.users,
     dates = cleanArguments.argv.dates,
     mbtilesPath = cleanArguments.argv.mbtiles,
     tmpGeojson = cleanArguments.tmpGeojson,
     tagFilter = cleanArguments.argv.filter,
-    bbox = cleanArguments.argv.bbox,
     maxWorkers = cleanArguments.argv.maxWorkers,
     tiles = cleanArguments.argv.tiles,
-    osmID = new Set(),
-    tmpFd;
+    resultJSON = {};
 
-if ((!geojson && !count) || !mbtilesPath || argv.help) {
+if ((!tmpGeojson && !count) || !mbtilesPath || argv.help) {
     help();
 }
 
@@ -36,37 +36,178 @@ tileReduce({
     map: path.join(__dirname, 'map.js'),
     sources: [{name: 'osm', mbtiles: mbtilesPath}],
     maxWorkers: maxWorkers,
-    bbox: bbox,
     tiles: tiles,
-    mapOptions: {
-        'count': count,
-        'tmpGeojson': tmpGeojson,
-        'dates': dates,
-        'users': users,
-        'tagFilter': tagFilter
-    }
+    mapOptions: { count, tmpGeojson, dates, users, tagFilter, mbtilesPath }
 })
 .on('start', function () {
-    if (tmpGeojson) {
-        tmpFd = fs.openSync(tmpGeojson, 'w');
-    }
 })
-.on('reduce', function (id) {
-    if (count && id) {
-        id.forEach(function (idElement) {
-            osmID.add(idElement);
-        });
-    }
+.on('reduce', function (results) {
+    Object.keys(results).forEach((key) => {
+        switch (key) {
+        case 'filter1.json':
+            resultJSON['filter1.json'] = results[key].features.reduce((final, feature) => {
+              if (!feature) return final;
+              if (feature.properties['@version'] < 5) {
+                if (!final[`@version${feature.properties['@version']}`]) {
+                    final[`@version${feature.properties['@version']}`] = {};
+                }
+                if (!final[`@version${feature.properties['@version']}`][feature.properties.highway]) {
+                    final[`@version${feature.properties['@version']}`][feature.properties.highway] = {
+                        count: 1,
+                        length: turf.length(feature)
+                    };
+                }
+                if (final[`@version${feature.properties['@version']}`] && final[`@version${feature.properties['@version']}`][feature.properties.highway]) {
+                    final[`@version${feature.properties['@version']}`][feature.properties.highway].count++;
+                    final[`@version${feature.properties['@version']}`][feature.properties.highway].length += turf.length(feature);
+                }
+              } else {
+                if (!final[`@version5Plus`]) {
+                    final[`@version5Plus`] = {};
+                }
+                if (!final[`@version5Plus`][feature.properties.highway]) {
+                    final[`@version5Plus`][feature.properties.highway] = {
+                        count: 1,
+                        length: turf.length(feature)
+                    };
+                } else {
+                    final[`@version5Plus`][feature.properties.highway].count++;
+                    final[`@version5Plus`][feature.properties.highway].length += turf.length(feature);
+                }
+              }
+                return final;
+            }, resultJSON['filter1.json'] ? resultJSON['filter1.json'] : {});
+            break;
+        case 'filter2.json':
+            resultJSON['filter2.json'] = results[key].features.reduce((final, feature) => {
+                if (!feature) return final;
+                if (!final[feature.properties.highway]) {
+                    final[feature.properties.highway] = {
+                        count: 1,
+                        length: turf.length(feature)
+                    };
+                } else {
+                    final[feature.properties.highway].count++;
+                    final[feature.properties.highway].length += turf.length(feature);
+                }
+                return final;
+            }, resultJSON['filter2.json'] ? resultJSON['filter2.json'] : {});
+            break;
+        case 'filter3.json':
+            resultJSON['filter3.json'] = results[key].features.reduce((final, feature) => {
+                if (!feature) return final;
+                if (!final[feature.properties.highway]) {
+                    final[feature.properties.highway] = {
+                        count: 1,
+                        length: turf.length(feature)
+                    };
+                } else {
+                    final[feature.properties.highway].count++;
+                    final[feature.properties.highway].length += turf.length(feature);
+                }
+                return final;
+            }, resultJSON['filter3.json'] ? resultJSON['filter3.json'] : {});
+            break;
+        case 'filter4.json':
+            resultJSON['filter4.json'] = results[key].features.reduce((final, feature) => {
+                if (!feature) return final;
+                if (!final[feature.properties.highway]) {
+                    final[feature.properties.highway] = {
+                        count: 1,
+                        length: turf.length(feature)
+                    };
+                } else {
+                    final[feature.properties.highway].count++;
+                    final[feature.properties.highway].length += turf.length(feature);
+                }
+                return final;
+            }, resultJSON['filter4.json'] ? resultJSON['filter4.json'] : {});
+            break;
+        case 'filter5.json':
+            resultJSON['filter5.json'] = results[key].features.reduce((final, feature) => {
+                if (!feature) return final;
+                if (!final[feature.properties.amenity]) {
+                    final[feature.properties.amenity] = {
+                        count: 1
+                    };
+                } else {
+                    final[feature.properties.amenity].count++;
+                }
+                return final;
+            }, resultJSON['filter5.json'] ? resultJSON['filter5.json'] : {});
+            break;
+        case 'filter6.json':
+            resultJSON['filter6.json'] = results[key].features.reduce((final, feature) => {
+                if (!feature) return final;
+                if (!final[feature.properties.building]) {
+                    final[feature.properties.building] = {
+                        count: 1
+                    };
+                } else {
+                    final[feature.properties.building].count++
+                }
+                return final;
+            }, resultJSON['filter6.json'] ? resultJSON['filter6.json'] : {});
+            break;
+        case 'filter7.json':
+            resultJSON['filter7.json'] = results[key].features.reduce((final, feature) => {
+                if (!feature) return final;
+                if (feature.properties['@version'] < 5) {
+                  if (!final[`@version${feature.properties['@version']}`]) {
+                      final[`@version${feature.properties['@version']}`] = {
+                          count: 1
+                      };
+                  } else {
+                      final[`@version${feature.properties['@version']}`].count++;
+                  }
+                }
+                else {
+                  if (!final[`@version5Plus`]) {
+                      final[`@version5Plus`] = {
+                        count: 1
+                      };
+                  } else {
+                      final[`@version5Plus`].count++;
+                  }
+                }
+                return final;
+            }, resultJSON['filter7.json'] ? resultJSON['filter7.json'] : {});
+            break;
+        }
+    });
 })
 .on('end', function () {
-    if (count) {
-        console.log('Features total: %d', osmID.size);
-    }
-    if (geojson) {
-        gsm(tmpGeojson, geojson, function () {
-            fs.closeSync(tmpFd);
-            fs.unlinkSync(tmpGeojson);
-            fs.rmdirSync(tmpDir);
-        });
-    }
+    const s3 = new AWS.S3();
+    Object.keys(resultJSON).forEach((key) => {
+      const s3Options = {
+        Bucket: 'aruna-information-seeding',
+        Key: `processed/${process.env.year}-Q${process.env.q}/json/${key}/${process.env.tileset}.json`
+      }
+      fs.writeFileSync(`${__dirname}/${process.env.tileset}-${key}.json`, JSON.stringify(resultJSON[key]));
+      // s3.putObject(Object.assign(s3Options, {
+      //   Body: JSON.stringify(resultJSON[key])
+      // }))
+      // .promise()
+      // .then(() => console.log(`Wrote file to s3://aruna-information-seeding/processed/${process.env.year}-Q${process.env.q}/json/${key}/${process.env.tileset}.json`))
+      // .catch((err) => {
+      //   throw err
+      // });
+    });
+});
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
+  const s3 = new AWS.S3();
+  const s3Options = {
+    Bucket: 'aruna-information-seeding',
+    Key: `errors/index/${process.env.year}-Q${process.env.q}-${process.env.tileset}.json`
+  }
+  s3.putObject(Object.assign(s3Options, {
+    Body: JSON.stringify(err, Object.getOwnPropertyNames(err))
+  }))
+  .promise()
+  .catch((err) => {
+    console.log(err);
+  });
+  process.exit(1);
 });
--- map.js
+++ map.js
@@ -1,53 +1,181 @@
 'use strict';
 
-var fs = require('fs');
-var ff = require('feature-filter');
-var featureCollection = require('turf-featurecollection');
+const fs = require('fs');
+const ff = require('feature-filter');
+const turf = require('turf');
+const polygonClipping = require('martinez-polygon-clipping');
+const turfLineSplit = require('@turf/line-split');
+const tilebelt = require('@mapbox/tilebelt');
+const whichPolygon = require('which-polygon');
+const countyFile = require('../counties.tiger2017.json');
 
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+let callback;
+
+const getPolygons = (feature) => {
+    switch(feature.geometry.type) {
+      case 'Polygon':
+        return turf.helpers.featureCollection([feature]);
+      case 'MultiPolygon':
+        const polygons = turf.helpers.featureCollection([]);
+        feature.geometry.coordinates.forEach((coordinates) => {
+          polygons.features.push(turf.helpers.feature({
+            type: "Polygon",
+            coordinates
+          }, feature.properties));
+        });
+        return polygons;
+    }
+};
+const splitFeatures = (geojson, tile, features) => {
+    const tilePolygon = tilebelt.tileToGeoJSON(tile);
+    const tileGeojsonGeometryIntersection = polygonClipping.intersection(geojson.geometry.coordinates,
+      tilePolygon.coordinates);
+    if (!tileGeojsonGeometryIntersection[0][0].length || !tileGeojsonGeometryIntersection[0][0][0].length) return {};
+    else {
+      const geojsonFeatureIntersectingWithTile = turf.helpers.multiPolygon(tileGeojsonGeometryIntersection);
+      geojsonFeatureIntersectingWithTile.properties = geojson.properties;
+      const result = {};
+      result[geojsonFeatureIntersectingWithTile.properties.GEOID] = {
+        Type: 'FeatureCollection',
+        features: []
+      };
+      features.forEach((mbtileFeature) => {
+        getPolygons(geojsonFeatureIntersectingWithTile).features.forEach((geojsonFeaturePolygon) => {
+          switch((mbtileFeature.geometry.type)) {
+            case 'LineString':
+              if ((turf.booleanWithin(mbtileFeature, geojsonFeaturePolygon))) {
+                result[geojsonFeaturePolygon.properties.GEOID].features =
+                result[geojsonFeaturePolygon.properties.GEOID].features.concat(mbtileFeature);
+              } else {
+                let split = turfLineSplit(mbtileFeature, geojsonFeaturePolygon);
+                let oddPair;
+                if(turf.booleanPointInPolygon(turf.point(mbtileFeature.geometry.coordinates[0]), geojsonFeaturePolygon)){
+                  oddPair = 0;
+                } else {
+                  oddPair = 1;
+                }
+                split.features.forEach((splitedPart, i) => {
+                  if((i + oddPair)%2 === 0) {
+                    result[geojsonFeaturePolygon.properties.GEOID].features =
+                    result[geojsonFeaturePolygon.properties.GEOID].features.concat(turf.helpers.feature({
+                      type: 'LineString',
+                      geometry: splitedPart.geometry,
+                      properties: mbtileFeature.properties
+                    }));
+                  }
+                });
+              }
+            break;
+            case 'Point':
+              if (turf.booleanPointInPolygon(mbtileFeature, geojsonFeaturePolygon)) {
+                result[geojsonFeaturePolygon.properties.GEOID].features.push(mbtileFeature);
+              }
+            break;
+            case 'MultiLineString':
+              const lineStrings = turf.lineSegment(mbtileFeature);
+              lineStrings.features.forEach((feature) => {
+                if ((turf.booleanWithin(feature, geojsonFeaturePolygon))) {
+                  feature.properties = mbtileFeature.properties;
+                  result[geojsonFeaturePolygon.properties.GEOID].features =
+                  result[geojsonFeaturePolygon.properties.GEOID].features.concat(feature);
+                }
+              });
+              mbtileFeature.geometry.coordinates.forEach(part => {
+                let split = turfLineSplit(turf.lineString(part), geojsonFeaturePolygon);
+                let oddPair;
+                if(turf.booleanPointInPolygon(turf.point(part[0]), geojsonFeaturePolygon)){
+                  oddPair = 0;
+                } else {
+                  oddPair = 1;
+                }
+                split.features.forEach((splitedPart, i) => {
+                  if((i + oddPair)%2 === 0) {
+                    result[geojsonFeaturePolygon.properties.GEOID].features =
+                    result[geojsonFeaturePolygon.properties.GEOID].features.concat(turf.helpers.feature({
+                      type: 'LineString',
+                      geometry: splitedPart.geometry,
+                      properties: mbtileFeature.properties
+                    }));
+                  }
+                });
+              });
+
+            break;
+            case 'Polygon':
+            case 'MultiPolygon':
+              let intersection;
+              try {
+                 intersection = polygonClipping.intersection(geojsonFeaturePolygon.geometry.coordinates,
+                  mbtileFeature.geometry.coordinates);
+                } catch (e) {
+                  try {
+                    intersection = polygonClipping.intersection(mbtileFeature.geometry.coordinates, geojsonFeaturePolygon.geometry.coordinates);
+                  } catch (e) {
+                      throw e;
+                  }
+                }
+                if (intersection) {
+                  result[geojsonFeaturePolygon.properties.GEOID].features =
+                  result[geojsonFeaturePolygon.properties.GEOID].features.concat(turf.helpers.feature({
+                    type: "MultiPolygon",
+                    coordinates: intersection
+                  }, mbtileFeature.properties));
+                }
+            break;
+          };
+        });
+      });
+      return result;
+
+    }
+};
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
+  const s3 = new AWS.S3();
+  const s3Options = {
+    Bucket: 'aruna-information-seeding',
+    Key: `errors/map/${process.env.year}-Q${process.env.q}-${process.env.tileset}.json`
+  }
+  s3.putObject(Object.assign(s3Options, {
+    Body: JSON.stringify(err, Object.getOwnPropertyNames(err))
+  }))
+  .promise()
+  .catch((err) => {
+    console.log(err);
+  });
+  callback();
+});
 /**
  * Filters the OSM features in a single tile based on given filter.
  * @module
  * @type {number}
  */
 module.exports = function (data, tile, writeData, done) {
+    callback = done;
+    const countyFeatureId = /\d+\.mbtiles/.exec(mapOptions.mbtilesPath)[0].split('.mbtiles')[0];
+    const countyFeature = countyFile.features.filter((feature) => feature.properties.GEOID === countyFeatureId)[0];
+    const callbackResult = {};
+    mapOptions.tagFilter.split(',').forEach((filter) => {
+        const featureFilter = ff(JSON.parse(fs.readFileSync(`${__dirname}/${filter}`)));
+        var layer = data.osm.osm;
+        var osmID = (mapOptions.count) ? [] : null;
+        var dates = Boolean(mapOptions.dates) ? parseDates(mapOptions.dates) : false;
+        var users = mapOptions.users;
 
-    var filter = (mapOptions.tagFilter) ? ff(mapOptions.tagFilter) : false;
-    var layer = data.osm.osm;
-    var osmID = (mapOptions.count) ? [] : null;
-    var dates = Boolean(mapOptions.dates) ? parseDates(mapOptions.dates) : false;
-    var users = mapOptions.users;
-    var result = layer.features.filter(function (val) {
-
-        if ((!users || (users && users.indexOf(val.properties['@user']) > -1)) && (
-            !mapOptions.dates || (mapOptions.dates && val.properties['@timestamp'] && val.properties['@timestamp'] >= dates[0] && val.properties['@timestamp'] <= dates[1])) && (!filter || (filter && filter(val)))) {
-
-            if (mapOptions.count) {
-                osmID.push(val.properties['@id']);
+        var result = layer.features.filter(function (val) {
+            if ((!users || (users && users.indexOf(val.properties['@user']) > -1)) && (
+                !mapOptions.dates || (mapOptions.dates && val.properties['@timestamp'] && val.properties['@timestamp'] >= dates[0] && val.properties['@timestamp'] <= dates[1])) && (!featureFilter || (featureFilter && featureFilter(val)))) {
+                return true;
             }
-
-            return true;
-        }
+        });
+        const splitFeaturesFinalList = splitFeatures(countyFeature, tile, result)[countyFeature.properties.GEOID];
+        console.log(JSON.stringify(splitFeaturesFinalList));
+        callbackResult[filter.split('../feature-filters/')[1]] = splitFeaturesFinalList;
     });
 
-    if (mapOptions.tmpGeojson && result.length > 0) {
-        var fc = featureCollection(result);
-        fs.appendFileSync(mapOptions.tmpGeojson, JSON.stringify(fc) + '\n');
-    }
-    done(null, osmID);
+    done(null, callbackResult);
 };
-
-/**
- @function parseDates
- @description Convert Date to timestamp. If endDate is not present, it is set as next immediate date to startDate.
- @param {string[]} strings
- @return {number[]}
- */
-function parseDates(dates) {
-    var startDate = new Date(dates[0]);
-    var endDate = new Date(dates[dates.length - 1]);
-    if (dates.length === 1) {
-        endDate.setDate((endDate.getDate() + 1));
-    }
-    //_timestamp in QA tiles is in seconds and not milliseconds
-    return [(startDate.getTime() / 1000), (endDate.getTime() / 1000)];
-}
