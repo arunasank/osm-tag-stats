0a90f977121562afbc16b50cb5d926f850b23e78
Aruna Sankaranarayanan <aruna.evam@gmail.com>
2019-03-30 01:45:06 +0530

    simplify

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const count = cleanArguments.argv.count,
     tagFilter = cleanArguments.argv.filter,
     maxWorkers = cleanArguments.argv.maxWorkers,
     tiles = cleanArguments.argv.tiles,
-    resultJSON = {};
+    resultJSON = {
+      filter1.json: {
+        count: 0,
+        length: 0
+      }
+    };
 
 if (!mbtilesPath || argv.help) {
     help();
@@ -39,154 +44,25 @@ tileReduce({
 .on('start', function () {
 })
 .on('reduce', function (results) {
-    Object.keys(results).forEach((key) => {
-        switch (key) {
-        case 'filter1.json':
-            resultJSON['filter1.json'] = results[key].features.reduce((final, feature) => {
-              if (!feature) return final;
-              if (feature.properties['@version'] < 5) {
-                if (!final[`@version${feature.properties['@version']}`]) {
-                    final[`@version${feature.properties['@version']}`] = {};
-                }
-                if (!final[`@version${feature.properties['@version']}`][feature.properties.highway]) {
-                    final[`@version${feature.properties['@version']}`][feature.properties.highway] = {
-                        count: 1,
-                        length: turf.length(feature)
-                    };
-                }
-                if (final[`@version${feature.properties['@version']}`] && final[`@version${feature.properties['@version']}`][feature.properties.highway]) {
-                    final[`@version${feature.properties['@version']}`][feature.properties.highway].count++;
-                    final[`@version${feature.properties['@version']}`][feature.properties.highway].length += turf.length(feature);
-                }
-              } else {
-                if (!final[`@version5Plus`]) {
-                    final[`@version5Plus`] = {};
-                }
-                if (!final[`@version5Plus`][feature.properties.highway]) {
-                    final[`@version5Plus`][feature.properties.highway] = {
-                        count: 1,
-                        length: turf.length(feature)
-                    };
-                } else {
-                    final[`@version5Plus`][feature.properties.highway].count++;
-                    final[`@version5Plus`][feature.properties.highway].length += turf.length(feature);
-                }
-              }
-                return final;
-            }, resultJSON['filter1.json'] ? resultJSON['filter1.json'] : {});
-            break;
-        case 'filter2.json':
-            resultJSON['filter2.json'] = results[key].features.reduce((final, feature) => {
-                if (!feature) return final;
-                if (!final[feature.properties.highway]) {
-                    final[feature.properties.highway] = {
-                        count: 1,
-                        length: turf.length(feature)
-                    };
-                } else {
-                    final[feature.properties.highway].count++;
-                    final[feature.properties.highway].length += turf.length(feature);
-                }
-                return final;
-            }, resultJSON['filter2.json'] ? resultJSON['filter2.json'] : {});
-            break;
-        case 'filter3.json':
-            resultJSON['filter3.json'] = results[key].features.reduce((final, feature) => {
-                if (!feature) return final;
-                if (!final[feature.properties.highway]) {
-                    final[feature.properties.highway] = {
-                        count: 1,
-                        length: turf.length(feature)
-                    };
-                } else {
-                    final[feature.properties.highway].count++;
-                    final[feature.properties.highway].length += turf.length(feature);
-                }
-                return final;
-            }, resultJSON['filter3.json'] ? resultJSON['filter3.json'] : {});
-            break;
-        case 'filter4.json':
-            resultJSON['filter4.json'] = results[key].features.reduce((final, feature) => {
-                if (!feature) return final;
-                if (!final[feature.properties.highway]) {
-                    final[feature.properties.highway] = {
-                        count: 1,
-                        length: turf.length(feature)
-                    };
-                } else {
-                    final[feature.properties.highway].count++;
-                    final[feature.properties.highway].length += turf.length(feature);
-                }
-                return final;
-            }, resultJSON['filter4.json'] ? resultJSON['filter4.json'] : {});
-            break;
-        case 'filter5.json':
-            resultJSON['filter5.json'] = results[key].features.reduce((final, feature) => {
-                if (!feature) return final;
-                if (!final[feature.properties.amenity]) {
-                    final[feature.properties.amenity] = {
-                        count: 1
-                    };
-                } else {
-                    final[feature.properties.amenity].count++;
-                }
-                return final;
-            }, resultJSON['filter5.json'] ? resultJSON['filter5.json'] : {});
-            break;
-        case 'filter6.json':
-            resultJSON['filter6.json'] = results[key].features.reduce((final, feature) => {
-                if (!feature) return final;
-                if (!final[feature.properties.building]) {
-                    final[feature.properties.building] = {
-                        count: 1
-                    };
-                } else {
-                    final[feature.properties.building].count++
-                }
-                return final;
-            }, resultJSON['filter6.json'] ? resultJSON['filter6.json'] : {});
-            break;
-        case 'filter7.json':
-            resultJSON['filter7.json'] = results[key].features.reduce((final, feature) => {
-                if (!feature) return final;
-                if (feature.properties['@version'] < 5) {
-                  if (!final[`@version${feature.properties['@version']}`]) {
-                      final[`@version${feature.properties['@version']}`] = {
-                          count: 1
-                      };
-                  } else {
-                      final[`@version${feature.properties['@version']}`].count++;
-                  }
-                }
-                else {
-                  if (!final[`@version5Plus`]) {
-                      final[`@version5Plus`] = {
-                        count: 1
-                      };
-                  } else {
-                      final[`@version5Plus`].count++;
-                  }
-                }
-                return final;
-            }, resultJSON['filter7.json'] ? resultJSON['filter7.json'] : {});
-            break;
+    results['filter1.json'].features.forEach((feature) => {
+        if (feature) {
+          resultJSON['filter1.json'].count++;
+          resultJSON['filter1.json'].length += turf.length(feature);
         }
     });
 })
 .on('end', function () {
     const s3 = new AWS.S3();
-    Object.keys(resultJSON).forEach((key) => {
-      const s3Options = {
-        Bucket: 'aruna-information-seeding',
-        Key: `processed/${process.env.year}-Q${process.env.q}/json/${key}/${process.env.tileset}.json`
-      }
-      s3.putObject(Object.assign(s3Options, {
-        Body: JSON.stringify(resultJSON[key])
-      }))
-      .promise()
-      .catch((err) => {
-        throw err
-      });
+    const s3Options = {
+      Bucket: 'aruna-information-seeding',
+      Key: `processed/all-highways/${process.env.year}-Q${process.env.q}/json/filter1.json/${process.env.tileset}.json`
+    }
+    s3.putObject(Object.assign(s3Options, {
+      Body: JSON.stringify(resultJSON['filter1.json'])
+    }))
+    .promise()
+    .catch((err) => {
+      throw err
     });
 });
 
